2176a52b6e601f2c5d334bc1fae5ca82223e7da5
Michael Wang <michael.wang@heygen.com>
2025-12-16 16:36:39 -0800

    add oauth 2

---
+++ credentials/HeyGenOAuth2Api.credentials.ts
@@ -0,0 +1,47 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class HeyGenOAuth2Api implements ICredentialType {
+	name = 'heyGenOAuth2Api';
+	displayName = 'HeyGen OAuth2 API';
+	extends = ['oAuth2Api'];
+	documentationUrl = 'https://docs.heygen.com/docs/heygen-oauth';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Grant Type',
+			name: 'grantType',
+			type: 'hidden',
+			default: 'authorizationCode',
+		},
+		{
+			displayName: 'Authorization URL',
+			name: 'authUrl',
+			type: 'hidden',
+			default: 'https://app.heygen.com/oauth/authorize',
+		},
+		{
+			displayName: 'Access Token URL',
+			name: 'accessTokenUrl',
+			type: 'hidden',
+			default: 'https://api2.heygen.com/v1/oauth/token',
+		},
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden',
+			default: '',
+		},
+		{
+			displayName: 'Auth URI Query Parameters',
+			name: 'authQueryParameters',
+			type: 'hidden',
+			default: '',
+		},
+		{
+			displayName: 'Authentication',
+			name: 'authentication',
+			type: 'hidden',
+			default: 'body',
+		},
+	];
+}
+
